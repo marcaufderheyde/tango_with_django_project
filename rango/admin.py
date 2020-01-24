d11307d75e3fd93ac9820325dd249b6f5cc70adc
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-01-24 22:18:03 +0000

    Successful completion of Chapter 5 with all tests passed

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, Question, Choice
 
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,13 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -19,3 +23,26 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
